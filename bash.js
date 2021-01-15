d325e9c9aafefca3eb45903b3071cdd037478d18
Ezgi Sirip <ezgisirip@gmail.com>
2021-01-15 16:35:58 -0500

    updates changes

--- bash.js
+++ bash.js
@@ -1,17 +1,6 @@
-process.stdout.write('prompt > ');
 
-process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim();
-    
-    if(cmd === 'pwd'){
 
-        process.stdout.write(`${process.cwd()}`)
+const { pwd } = require("./pwd");
 
-    }
-
-    process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
-
-    
-})
+const { fs }  = require('./ls');
 
